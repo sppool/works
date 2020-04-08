957f1bd2a55ff3ac46773b118d50172a3ae5a445
Pool <sppool.me98gNCTU.edu.tw>
2020-04-08 21:34:14 +0800

    COVID-19

--- 3blue1brown/COIVD-19/COVID-19.py
+++ 3blue1brown/COIVD-19/COVID-19.py
@@ -10,24 +10,27 @@ def update_coor_mov_status(coor, mov, status):
     r = coor[np.bitwise_and(status > 0, status < 500)]  # 病人座標
     if len(g) and len(r):  # 當病人或是健康人數為0 不在更新
         d_arr = manhattan_distances(r, g)  # 計算病人和健康人距離(array)
-        d = 0.002  # 舉離設定
+        d = 0.002  # 距離設定
+        # 距離太近就傳染 status轉換為1(病人)
         g2r = np.any(d_arr < d, 0).astype(np.int8)
         status[status == 0] += g2r
 
     mov[np.bitwise_and(status >= 500, status < 600)] *= 0.98  # 死亡動畫變慢
-    coor = coor + mov
+    coor = coor + mov  # 更新位置
+    # 碰撞轉換
     upper = coor > 1
     lower = coor < 0
     coor[upper] = 2 - coor[upper]
     coor[lower] = -coor[lower]
+    # 碰撞向量轉換
     mov[upper] = mov[upper] * -1
     mov[lower] = mov[lower] * -1
 
     return coor, mov, status
 
 
-n = 1000  # 數目
-status = np.zeros(n)  # 狀態設定 初始0號病人
+n = 1000  # 模擬人數
+status = np.zeros(n)  # 狀態設定 初始0號病人 (0:健康人 >0:病人)
 status[0] = 1
 angle = np.random.uniform(0, 2 * np.pi, n)  # 移動角度
 speed = np.random.uniform(0.5, 1, n)  # 移動速度
@@ -51,9 +54,9 @@ def init():
 
 def update(frame):
     global coor, mov, status
-    g_coor = coor[status == 0]
-    y_coor = coor[np.bitwise_and(status > 0, status < 50)]
-    r_coor = coor[np.bitwise_and(status > 0, status < 500)]
+    g_coor = coor[status == 0]  # 健康人座標
+    y_coor = coor[np.bitwise_and(status > 0, status < 50)]  # 中獎人座標
+    r_coor = coor[np.bitwise_and(status > 0, status < 500)]  # 病人座標
     k_coor = coor[np.bitwise_and(status >= 500, status < 600)]  # 死亡動畫消失
     p1.set_data(g_coor[:, 0], g_coor[:, 1])
     p2.set_data(y_coor[:, 0], y_coor[:, 1])
@@ -65,5 +68,5 @@ def update(frame):
 
 
 ani = FuncAnimation(fig=fig, func=update, frames=1,
-                    init_func=init, interval=20, blit=False)
+                    init_func=init, interval=20, blit=False)  # fps=50
 plt.show()
