7d0a3dde39c4b9c3c15e5b7e4a64ef9bfd5b9c39
Pool <sppool.me98gNCTU.edu.tw>
2020-04-11 20:35:42 +0800

    v2.151

--- 3blue1brown/COIVD-19/COVID-19.py
+++ 3blue1brown/COIVD-19/COVID-19.py
@@ -11,7 +11,7 @@ status  狀態值解釋
 501~600 : 死亡or隔離(無傳染力), 單純是畫面顯示需要 (正常更新時+1)
 601     : 不再更新狀態
 
-r (float) (約0.001~0.003) 接觸距離(半徑)
+r (float) (約0.001~0.002) 接觸距離(半徑)
 n (int) 點位數量 (1000) 太少可以增加 r 提高接觸機會
 
 初始感染率 : 1 - ((1 - (r**2) * np.pi)**(n - 1))**450
@@ -19,6 +19,7 @@ n (int) 點位數量 (1000) 太少可以增加 r 提高接觸機會
 
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 from matplotlib.animation import FuncAnimation
 from sklearn.metrics.pairwise import manhattan_distances  # 曼哈頓距離可以省大量計算
 
@@ -48,8 +49,12 @@ def update_coor_mov_status(coor, mov, status):
 
 
 n = 1000  # 模擬人數(太多很爽 但是會lag 吃光記憶體會當機 請緩慢增加)
-r = 0.002  # 距離設定 微調影響很顯著 約0.001~0.003
+r = 0.0013  # 距離設定 微調影響很顯著 約0.001~0.002
 d = r * (np.pi / 2)**0.5  # 轉換成曼哈頓距離(讓面積相等) 可以減少計算 且已經足夠
+# 初始機率 0.75~0.95 (r 0.001 ~ 0.0015)有明顯差距
+print(f'p: {1 - ((1 - (r**2) * np.pi)**(n - 1))**450}')
+time.sleep(2)
+
 # 狀態設定 初始0號病人 (0:健康人, 1~500:病人, 500~:凋零不再傳染)
 status = np.zeros(n).astype(np.int)
 status[0] = 1  # 零號COVID-19病患
@@ -62,7 +67,7 @@ mov = mov.astype(np.float32)  # 節省資源 設定 np.float16 會不夠
 coor = np.random.uniform(0, 1, size=(n, 2))  # 位置座標
 coor = coor.astype(np.float32)  # 節省資源
 
-fig_size = 8  # init圖形大小
+fig_size = 9  # init圖形大小
 fig, ax = plt.subplots(figsize=(fig_size, fig_size))
 p4, = ax.plot('', '', 'o', ms=3, c='#404040', alpha=0.5)  # 最低圖層
 p2, = ax.plot('', '', 'o', ms=15, c='#ffa000', alpha=0.7)
